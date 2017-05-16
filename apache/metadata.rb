6085481bdcd673440280f0008d147e1d96c37e80
MANOJ <manojbasani@gmail.com>
2017-05-16 08:51:22 -0500

    added chef apache cookbook

---
+++ apache/metadata.rb
@@ -0,0 +1,10 @@
+name             'apache'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures apache'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+issues_url 'https://github.com/chef-cookbooks/something/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/something' if respond_to?(:source_url)
